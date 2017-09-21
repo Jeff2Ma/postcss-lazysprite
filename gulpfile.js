9e09ab02ee3ce0cd67ab522765485e127a14d94f
Jeff2Ma <mjh2316@gmail.com>
2017-09-21 14:33:05 +0800

    Rewrite with ES6.

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,55 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var lazysprite = require('./index.js');
-var mocha = require('gulp-mocha');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const lazysprite = require('./index.js')
+const mocha = require('gulp-mocha')
+const sourcemaps = require('gulp-sourcemaps')
 
-var files = ['index.js'];
-var watchFiles = ['index.js', 'gulpfile.js', 'examples/src/**/**', 'test/src/**/**'];
+const files = ['index.js']
+const watchFiles = ['index.js', 'gulpfile.js', 'examples/src/**/**', 'test/src/**/**']
 
 gulp.task('lint', function () {
-	var eslint = require('gulp-eslint');
-	return gulp.src(files)
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
+  const eslint = require('gulp-eslint')
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
 
 gulp.task('examples', function () {
-	return gulp.src('examples/*.js', {read: false})
-		.pipe(mocha({timeout: 1000000}));
-});
+  return gulp.src('examples/*.js', {read: false})
+    .pipe(mocha({timeout: 1000000}))
+})
 
 gulp.task('htmlcopy', function () {
-	return gulp.src(['./examples/src/html/index.html'], {base: './examples/src/html/'})
-		.pipe(gulp.dest('./examples/dist/html/'));
-});
+  return gulp.src(['./examples/src/html/index.html'], {base: './examples/src/html/'})
+    .pipe(gulp.dest('./examples/dist/html/'))
+})
 
 gulp.task('test', function () {
-	return gulp.src('test/*.js', { read: false })
-		.pipe(mocha({ timeout: 1000000 }));
-});
+  return gulp.src('test/*.js', {read: false})
+    .pipe(mocha({timeout: 1000000}))
+})
 
 gulp.task('css', function () {
-	return gulp.src('./examples/src/css/**/*.css')
-		// .pipe(sourcemaps.init())
-		.pipe(postcss([lazysprite({
-			imagePath: './examples/src/slice',
-			stylesheetInput: './examples/src/css',
-			stylesheetRelative: './examples/dist/css',
-			spritePath: './examples/dist/sprites',
-			outputExtralCSS: true,
-			smartUpdate: true,
-			nameSpace: 'icon-',
-			logLevel: 'debug'
-		})]))
-		// .pipe(sourcemaps.write("."))
-		.pipe(gulp.dest('./examples/dist/css'));
-});
+  return gulp.src('./examples/src/css/**/*.css')
+  // .pipe(sourcemaps.init())
+    .pipe(postcss([lazysprite({
+      imagePath: './examples/src/slice',
+      stylesheetInput: './examples/src/css',
+      stylesheetRelative: './examples/dist/css',
+      spritePath: './examples/dist/sprites',
+      outputExtralCSS: true,
+      smartUpdate: true,
+      nameSpace: 'icon-',
+      logLevel: 'debug'
+    })]))
+    // .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest('./examples/dist/css'))
+})
 
-gulp.task('default', ['htmlcopy', 'css', 'watch']);
+gulp.task('default', ['htmlcopy', 'css', 'watch'])
 
 gulp.task('watch', function () {
-	// gulp.watch(watchFiles, ['css', 'test', 'lint']);
-	gulp.watch(watchFiles, ['css', 'lint']);
-});
+  // gulp.watch(watchFiles, ['css', 'test', 'lint']);
+  gulp.watch(watchFiles, ['css', 'lint'])
+})
